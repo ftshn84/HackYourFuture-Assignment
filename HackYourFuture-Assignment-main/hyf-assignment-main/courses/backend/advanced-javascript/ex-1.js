d334b878115cbdd2f405390ce8432eff93703c67
Fatima <ftshn84@gmail.com>
2026-03-08 16:05:22 +0100

    refactor: clean up console logs and organize filter functions for tea data

--- HackYourFuture-Assignment-main/hyf-assignment-main/courses/backend/advanced-javascript/ex-1.js
+++ HackYourFuture-Assignment-main/hyf-assignment-main/courses/backend/advanced-javascript/ex-1.js
@@ -16,6 +16,29 @@ data.forEach(tea => {
         unorganicCount++;//console.log(`${tea.name} is not organic.`);
     }
 });
-console.log("Number of organic teas:", organicCount);
-console.log("Number of non-organic teas:", unorganicCount);
+//console.log("Number of organic teas:", organicCount);
+//console.log("Number of non-organic teas:", unorganicCount);
 
+
+const names = data.map(function (tea) {
+    return tea.name
+})
+//console.log(names);
+//Part 3: filter
+//filter keeps items that match. Fewer items out, same shape.
+//Get all organic teas.
+const matchItem = data.filter(function (tea) {
+    return tea.organic === true;
+});
+//console.log(matchItem);
+//Get all teas from Japan.
+const japanTeas = data.filter(function (tea) {
+    return tea.origin == "Japan";
+})
+//console.log(japanTeas);
+//Exercise 9
+//Get all teas with caffeineLevel equal to "high"
+const caffeineLevelIeas = data.filter(function (tea) {
+    return tea.caffeineLevel === "high"
+})
+console.log(caffeineLevelIeas);
